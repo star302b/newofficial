4bf80899f7c56d17e3e27c8607ae87d887978dca
biserkov <alexey.biserkov@mvfglobal.com>
2023-01-24 22:53:34 +0100

    fix FE bugs

--- themes/dark-green/components/helpers/css-variables.php
+++ themes/dark-green/components/helpers/css-variables.php
@@ -31,10 +31,58 @@
     <style>
         /*@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");*/
         @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+        @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css');
+        
 
         body {
             font-family: 'Roboto', sans-serif;
             font-size: 18px;
+        }
+
+        .acc-content>div{
+            padding: 1rem !important;
+        }
+
+        .site-footer ul{
+            padding: 10px 0;
+        }
+
+        .modal-box .sign-area label,
+        .modal-box .sign-area .form-bottom a,
+        .modal-box .sign-area .form-bottom{
+            color: var(--text-color-light)
+        }
+
+        .sign-area button{
+            margin-top: 20px;
+        }
+
+        .acc-header:after{
+            font-family: "Font Awesome\ 5 Free" !important;
+        }
+
+        .section-form{
+            padding-left: 0 !important;
+            padding-right: 0 !important;
+        }
+
+        @media screen and (max-width: 767px) {
+            .steps-section--variant-1 .row {
+                margin: 7em 0 0;
+            }
+            .steps-section--variant-1 .col{
+                margin: 5em 0;
+            }
+
+            .lock-line svg{
+                flex: 0 0 20px;
+            }
+
+            .lock-line .align-center p{
+                font-size: 16px;
+                line-height: 1.2;
+                padding-left: 10px;
+            }
         }
 
     </style>
